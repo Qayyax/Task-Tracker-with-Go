f1623f6da9a92222da6d473fb9bfdb852badecd4
Qayyax <106759130+Qayyax@users.noreply.github.com>
2025-12-22 14:02:30 -0500

    successfully created a struct, and imported the struct package

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/Qayyax/Task-Tracker-with-Go/types"
+)
 
 func main() {
-	fmt.Println("This is working")
+	fmt.Println("Testing the Task struct")
+	// fmt.Println("Current time is: ", time.Now())
+	task1 := types.NewTask("First task", "Just to see if this works")
+	fmt.Println("Name", task1.Name)
+	fmt.Println("Description", task1.Description)
+	fmt.Println("CreatedAt", task1.CreatedAt)
 }
--- types/task.go
+++ types/task.go
@@ -1,4 +1,4 @@
-package main
+package types
 
 import "time"
 
@@ -9,12 +9,36 @@ import "time"
 // - `updatedAt`: The date and time when the task was last updated
 
 type Task struct {
-	id          int
-	description string
-	status      string // todo, in-progress, done
-	createdAt   time.Time
-	updatedAt   time.Time
+	Id          int
+	Name        string
+	Description string
+	Status      Status // [0] todo, [1] in-progress, [2] done
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
 }
+
+type Status int
+
+const (
+	Todo Status = iota
+	InProgress
+	Done
+)
 
 // TODO:
 // - [] methods for Task
+// - [] cli repl question that asks for name of new task and the description
+
+// This is not a method
+func NewTask(name string, description string) *Task {
+	// should check for last id in the json file
+
+	task := Task{
+		Id:          1, // change this to check for id
+		Name:        name,
+		Description: description,
+		Status:      Todo,
+		CreatedAt:   time.Now(),
+	}
+	return &task
+}
