75fa1c8aa3f1041434917ea9acc85620d9218ca2
Qayyax <106759130+Qayyax@users.noreply.github.com>
2025-12-22 14:46:22 -0500

    learnt how to read text from cli

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 
 	"github.com/Qayyax/Task-Tracker-with-Go/types"
 )
@@ -10,7 +12,16 @@ func main() {
 	fmt.Println("Testing the Task struct")
 	// fmt.Println("Current time is: ", time.Now())
 	task1 := types.NewTask("First task", "Just to see if this works")
-	fmt.Println("Name", task1.Name)
-	fmt.Println("Description", task1.Description)
-	fmt.Println("CreatedAt", task1.CreatedAt)
+	fmt.Println("Name from new Task", task1.Name)
+
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Print("What is your name? ")
+	scanner.Scan()
+	input := scanner.Text()
+
+	fmt.Printf("You entered: %s\n", input)
+
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading standard input:", err)
+	}
 }
